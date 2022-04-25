388305b7f08bdf31352d766348d5a984af640a3a
Jim Cooper <jim@cooperplanet.com>
2022-04-25 17:46:14 -0600

    Catalog page data bindings.

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product.model';
+
+@Component({
+  selector: 'bot-catalog',
+  templateUrl: './catalog.component.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class CatalogComponent implements OnInit {
+  product: IProduct;
+
+  constructor() { 
+    this.product = {
+      id: 2,
+      description:
+        "A friendly robot head with two eyes and a smile -- great for domestic use.",
+      name: "Friendly Bot",
+      imageName: "head-friendly.png",
+      category: "Heads",
+      price: 945.0,
+      discount: 0.2,
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+  getImageUrl(product: IProduct) { 
+    return '/assets/images/robot-parts/' + product.imageName;
+  }
+}
