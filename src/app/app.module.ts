f2ac073eb3708a5019b978f2a21fb8329e56c7cd
Joe Eames <joeeames@gmail.com>
2022-05-11 11:51:37 -0600

    added detail component to catalog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { SwitchExampleComponent } from './examples/switch-example/switch-example.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    HomeComponent, CatalogComponent, SiteHeaderComponent, SwitchExampleComponent
+    HomeComponent, CatalogComponent, SiteHeaderComponent, SwitchExampleComponent, ProductDetailsComponent
   ],
   imports: [
     BrowserModule
