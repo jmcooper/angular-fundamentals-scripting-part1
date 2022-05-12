325946ae0e5abfbec96ee4233c4a9efaa53d1927
Joe Eames <joeeames@gmail.com>
2022-05-12 14:02:08 -0600

    added sorting of the catalog

--- src/app/catalog/product.service.ts
+++ src/app/catalog/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { IProduct } from './product.model';
 
 @Injectable({
@@ -10,6 +10,14 @@ export class ProductService {
   constructor(private http: HttpClient) {}
 
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>('/api/products');
+    return this.http.get<IProduct[]>('/api/products').pipe(
+      map((c) => {
+        return c.sort((a, b) => {
+          if (a.name < b.name) return -1;
+          if (a.name === b.name) return 0;
+          return 1;
+        });
+      })
+    );
   }
 }
